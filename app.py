459f7321563af79e35f933e2572245298bcfc88b
stuartffh <91503509+stuartffh@users.noreply.github.com>
2025-07-04 20:38:04 -0300

    Remove duplicates and format

--- app.py
+++ app.py
@@ -1,53 +1,60 @@
-from flask import Flask, render_template, jsonify
 import os
+
 import urllib3
+from flask import Flask, jsonify, render_template
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 import requests
-from google.protobuf.json_format import MessageToDict
+from google.protobuf import message_factory
 from google.protobuf.descriptor_pb2 import FileDescriptorProto
 from google.protobuf.descriptor_pool import DescriptorPool
-from google.protobuf import message_factory
+from google.protobuf.json_format import MessageToDict
 
 app = Flask(__name__)
 
-DEBUG_REQUESTS = os.environ.get('DEBUG_REQUESTS', 'false').lower() in ('1', 'true', 'yes')
+DEBUG_REQUESTS = os.environ.get("DEBUG_REQUESTS", "false").lower() in (
+    "1",
+    "true",
+    "yes",
+)
 
 # Permite personalizar a verificação SSL via variável de ambiente
-VERIFY_SSL = os.environ.get('VERIFY_SSL', 'false').lower() not in ('false', '0', 'no')
+VERIFY_SSL = os.environ.get("VERIFY_SSL", "false").lower() not in ("false", "0", "no")
 
 url = "https://cgg.bet.br/casinogo/widgets/v2/live-rtp"
 headers = {
-    'accept': 'application/x-protobuf',
-    'content-type': 'application/x-protobuf',
-    'x-language-iso': 'pt-BR',
-    'origin': 'https://cgg.bet.br',
-    'referer': 'https://cgg.bet.br/pt-BR/casinos/casino/lobby'
+    "accept": "application/x-protobuf",
+    "content-type": "application/x-protobuf",
+    "x-language-iso": "pt-BR",
+    "origin": "https://cgg.bet.br",
+    "referer": "https://cgg.bet.br/pt-BR/casinos/casino/lobby",
 }
-data = b'\x08\x01\x10\x02'
+data = b"\x08\x01\x10\x02"
+
 
 def get_protobuf_message():
     proto_schema = FileDescriptorProto()
-    proto_schema.name = 'rtp.proto'
-    proto_schema.package = 'rtp'
+    proto_schema.name = "rtp.proto"
+    proto_schema.package = "rtp"
 
     game = proto_schema.message_type.add()
-    game.name = 'Game'
+    game.name = "Game"
 
-    game.field.add(name='id', number=1, type=3)
-    game.field.add(name='name', number=2, type=9)
+    game.field.add(name="id", number=1, type=3)
+    game.field.add(name="name", number=2, type=9)
     game_provider = game.nested_type.add()
-    game_provider.name = 'Provider'
-    game_provider.field.add(name='name', number=2, type=9)
-    game_provider.field.add(name='slug', number=5, type=9)
+    game_provider.name = "Provider"
+    game_provider.field.add(name="name", number=2, type=9)
+    game_provider.field.add(name="slug", number=5, type=9)
 
-    game.field.add(name='provider', number=3, type=11, type_name='.rtp.Game.Provider')
-    game.field.add(name='image', number=4, type=9)
-    game.field.add(name='rtp', number=5, type=5)
-    game.field.add(name='extra', number=6, type=4)  # campo adicional tratado
+    game.field.add(name="provider", number=3, type=11, type_name=".rtp.Game.Provider")
+    game.field.add(name="image", number=4, type=9)
+    game.field.add(name="rtp", number=5, type=5)
+    game.field.add(name="extra", number=6, type=4)  # campo adicional tratado
 
     response = proto_schema.message_type.add()
-    response.name = 'Response'
-    response.field.add(name='games', number=2, type=11, type_name='.rtp.Game', label=3)
+    response.name = "Response"
+    response.field.add(name="games", number=2, type=11, type_name=".rtp.Game", label=3)
 
     pool = DescriptorPool()
     file_desc = pool.Add(proto_schema)
@@ -55,18 +62,22 @@ def get_protobuf_message():
 
     return message_factory.GetMessageClass(response_desc)
 
+
 ProtobufMessage = get_protobuf_message()
+
 
 def decode_signed(value):
     if value > (1 << 63):
-        value -= (1 << 64)
+        value -= 1 << 64
     return value
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/api/games')
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/api/games")
 def games():
     if DEBUG_REQUESTS:
         print("\n[DEBUG] >>> Enviando Requisição <<<")
@@ -84,15 +95,15 @@ def games():
 
     decoded_message = ProtobufMessage()
     decoded_message.ParseFromString(response.content)
-    games = MessageToDict(decoded_message).get('games', [])
+    games = MessageToDict(decoded_message).get("games", [])
 
     for game in games:
-        extra = game.get('extra')
+        extra = game.get("extra")
         if extra is not None:
-            game['extra'] = decode_signed(int(extra))
-            game['rtp_status'] = 'down' if game['extra'] < 0 else 'up'
+            game["extra"] = decode_signed(int(extra))
+            game["rtp_status"] = "down" if game["extra"] < 0 else "up"
         else:
-            game['rtp_status'] = 'neutral'
+            game["rtp_status"] = "neutral"
 
     if DEBUG_REQUESTS:
         print("[DEBUG] <<< Mensagem Decodificada (JSON) >>>")
@@ -101,15 +112,16 @@ def games():
     return jsonify(games)
 
 
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     import argparse
+
     parser = argparse.ArgumentParser(add_help=False)
-    parser.add_argument('--insecure', action='store_false', help='Desativa verificação SSL')
+    parser.add_argument(
+        "--insecure", action="store_false", help="Desativa verificação SSL"
+    )
     args, _ = parser.parse_known_args()
     if args.insecure:
         VERIFY_SSL = False
 
-    debug_mode = os.environ.get('FLASK_DEBUG', 'false').lower() in ('1', 'true', 'yes')
+    debug_mode = os.environ.get("FLASK_DEBUG", "false").lower() in ("1", "true", "yes")
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=debug_mode)
